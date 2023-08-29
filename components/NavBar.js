4e577e9c53c93b95f94986c009d337dadf84949d
min199973 <min199973@gmail.com>
2023-08-29 22:54:45 +0900

    #1.0 ~ #1.7

---
+++ components/NavBar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link className={router.pathname === "/" ? "active" : ""} href="/">
+        Home
+      </Link>
+      <Link
+        className={router.pathname === "/about" ? "active" : ""}
+        href="/about"
+      >
+        about
+      </Link>
+      <style jsx global>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
+    </nav>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import NavBar from "@/components/NavBar";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </>
+  );
 }

36007d81645606c33d883d4349e3461463c8496b
min199973 <min199973@gmail.com>
2023-08-30 22:44:37 +0900

    NextJS #2.0 ~ #2.8

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import NavBar from "@/components/NavBar";
 import "../styles/globals.css";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
+import Seo from "../components/Seo";
+
 export default function Potato() {
   return (
     <div>
+      <Seo title="about" />
+      <title>Home | Next Movies</title>
       <h1>About</h1>
     </div>
   );
